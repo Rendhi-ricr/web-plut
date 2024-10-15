d3d1928cefb1b3e88a051dea6c9c634eb3ac91fd
ryzntx <thalalatha13@gmail.com>
2024-10-15 16:57:26 +0700

    Refactor admin user edit view

--- app/Views/admin/user/edit.php
+++ app/Views/admin/user/edit.php
@@ -1,6 +1,6 @@
-<?= $this->extend('layouts/base') ?>
-<?= $this->section('title') ?>Data User<?= $this->endSection() ?>
-<?= $this->section('content') ?>
+<?=$this->extend('layouts/base')?>
+<?=$this->section('title')?>Data User<?=$this->endSection()?>
+<?=$this->section('content')?>
 <div class="container-fluid p-0">
 
     <div class="mb-3">
@@ -10,7 +10,8 @@
             </div>
             <div class="col-md-6">
                 <div class="text-end">
-                    <a href="<?= base_url('admin/user') ?>" class="btn btn-primary"><i class="fa fa-arrow-left fa-sm"></i> Kembali</a>
+                    <a href="<?=base_url('admin/user')?>" class="btn btn-primary"><i class="fa fa-arrow-left fa-sm"></i>
+                        Kembali</a>
                 </div>
             </div>
         </div>
@@ -20,41 +21,43 @@
         <div class="col-12">
 
             <div class="card">
-                <form action="<?= base_url('admin/user/update/' . $user->id_user) ?>" method="post">
-                    <?= csrf_field() ?>
+                <form action="<?=base_url('admin/user/update/' . $user->id_user)?>" method="post">
+                    <?=csrf_field()?>
                     <div class="card-body">
                         <?php if (session()->getFlashdata('errors')): ?>
-                            <div class="alert alert-danger">
-                                <div class="alert-icon">
-                                    <i class="fa fa-circle-exclamation"></i>
-                                </div>
-                                <div class="alert-message">
-                                    Ada kesalahan dalam pengisian form:
-                                    <ul>
-                                        <?php foreach (session()->getFlashdata('errors') as $error): ?>
-                                            <li><?= $error ?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                </div>
+                        <div class="alert alert-danger">
+                            <div class="alert-icon">
+                                <i class="fa fa-circle-exclamation"></i>
                             </div>
-                        <?php endif; ?>
+                            <div class="alert-message">
+                                Ada kesalahan dalam pengisian form:
+                                <ul>
+                                    <?php foreach (session()->getFlashdata('errors') as $error): ?>
+                                    <li><?=$error?></li>
+                                    <?php endforeach;?>
+                                </ul>
+                            </div>
+                        </div>
+                        <?php endif;?>
                         <?php if (session()->getFlashdata('error')): ?>
-                            <div class="alert alert-danger">
-                                <div class="alert-icon">
-                                    <i class="fa fa-circle-exclamation"></i>
-                                </div>
-                                <div class="alert-message">
-                                    <?= session()->getFlashdata('error') ?>
-                                </div>
+                        <div class="alert alert-danger">
+                            <div class="alert-icon">
+                                <i class="fa fa-circle-exclamation"></i>
                             </div>
-                        <?php endif; ?>
+                            <div class="alert-message">
+                                <?=session()->getFlashdata('error')?>
+                            </div>
+                        </div>
+                        <?php endif;?>
                         <div class="mb-3">
                             <label for="nama" class="form-label">Nama</label>
-                            <input type="text" name="nama" id="nama" class="form-control" required value="<?= $user->nama ?? old('nama') ?>">
+                            <input type="text" name="nama" id="nama" class="form-control" required
+                                value="<?=$user->nama ?? old('nama')?>">
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" required value="<?= $user->email ?? old('email') ?>">
+                            <input type="email" name="email" id="email" class="form-control" required
+                                value="<?=$user->email ?? old('email')?>">
                         </div>
                         <div class="mb-3 row">
                             <div class="col-6">
@@ -70,8 +73,12 @@
                             <label for="role" class="form-label">Role</label>
                             <select name="role" id="role" class="form-control">
                                 <option value="" selected>Pilih Role</option>
-                                <option <?= $user->role == 'admin' ? 'selected ' : old('role') == 'admin' ? 'selected' : '' ?> value="admin">Admin</option>
-                                <option <?= $user->role == 'operator' ? 'selected' : old('role') == 'operator' ? 'selected' : '' ?> value="operator">Operator</option>
+                                <option
+                                    <?=$user->role == 'admin' ? 'selected ' : (old('role') == 'admin' ? 'selected' : '')?>
+                                    value="admin">Admin</option>
+                                <option
+                                    <?=$user->role == 'operator' ? 'selected' : (old('role') == 'operator' ? 'selected' : '')?>
+                                    value="operator">Operator</option>
                             </select>
                         </div>
                     </div>
@@ -88,7 +95,7 @@
 
 
 
-<?= $this->endSection() ?>
+<?=$this->endSection()?>
 
-<?= $this->section('script') ?>
-<?= $this->endSection() ?>
+<?=$this->section('script')?>
+<?=$this->endSection()?>
